aeda6f920ec05196f1a3306adcc2b9269a217dde
Stefan Hermelin <stefan.hermelin@tink.com>
2021-03-04 16:12:56 +0100

    Another update

--- scripts/mysql-to-s3.py
+++ scripts/mysql-to-s3.py
@@ -22,10 +22,10 @@ def mapping(_input: dict) -> dict:
 
 for _table_name in tables:
     connection_mysql8_options = {
-    "url": "jdbc:mysql://database-2.cgr3hkzl9dws.eu-west-1.rds.amazonaws.com:3306/glueextract",
+    "url": "",
     "dbtable": _table_name,
-    "user": "admin",
-    "password": "asdkjhqweiuy123",
+    "user": "",
+    "password": "",
     "customJdbcDriverS3Path": "s3://stefan-glue/gluelibs/mysql-connector-java-8.0.23.jar",
     "customJdbcDriverClassName": "com.mysql.cj.jdbc.Driver"}
     table: DynamicFrame = glueContext.create_dynamic_frame.from_options(connection_type="mysql",
